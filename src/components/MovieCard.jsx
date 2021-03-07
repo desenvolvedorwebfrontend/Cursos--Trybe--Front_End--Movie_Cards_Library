8c530ec7d671824418cc699a1e826ec168bc8acd
MarcosAlbuquerque <mpdsa20@gmail.com>
2021-03-06 23:48:20 -0300

    travado nos testes do Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,23 @@
-// implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+// import img1 from '../../public/images/Appleseed_Alpha.jpg';
+
+class MovieCard extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return (
+      <div>
+        <img src="images/movie_1" alt="" />
+        <h4>Movie Title 1</h4>
+        <h5>Movie Subtitle 1</h5>
+        <p>Movie Storyline 1</p>
+        <Rating className="rating" rating={ 4.5 } />
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import Rating from './Rating';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Componente MovieList</h1>
+        <MovieCard />
+        <Rating />
+      </div>
+    );
+  }
+}
+
+export default MovieList;
